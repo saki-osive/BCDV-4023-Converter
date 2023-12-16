083b5edc3d973c596816fc1f78a1789cce719441
arcturus <saki.osive@gmail.com>
2023-12-17 04:14:13 +0530

    Add dynamic prices conversion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
+import React from 'react';
 import { useEffect, useState } from 'react';
 import contract from './contract/contracts/Convertor.sol/Convertor.json';
-import {ethers} from 'ethers'
-import './App.css'
+import { ethers } from 'ethers';
+import './App.css';
 
-import React from 'react';
 import { makeStyles } from '@mui/styles';
-import { Typography, Radio, RadioGroup, FormControlLabel, CircularProgress, Paper } from '@mui/material';
+import {
+  Typography,
+  Card,
+  CardHeader,
+  CardContent,
+  RadioGroup,
+  FormControlLabel,
+  Radio,
+  CircularProgress,
+} from '@mui/material';
+import { teal, purple } from '@mui/material/colors';
+import {color} from "@mui/system";
 
 const contractAddress = '0x596983EbAE3183CBEB7beD4D420431cb0E62797f';
 const abi = contract.abi;
 
-
 const useStyles = makeStyles((theme) => ({
-  box: {
-    padding: theme.spacing(2),
-    margin: theme.spacing(2),
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100vh',
+  },
+  card: {
+    width: '400px',
+    marginBottom: theme.spacing(3),
   },
   radioButtons: {
     display: 'flex',
     flexDirection: 'column',
+    alignItems: 'center',
     marginTop: theme.spacing(2),
   },
   result: {
@@ -28,6 +46,7 @@ const useStyles = makeStyles((theme) => ({
   label1: {
     fontWeight: 'bold',
     marginBottom: theme.spacing(1),
+    color: purple[500],
   },
 }));
 
@@ -72,55 +91,53 @@ const App = () => {
     };
 
     fetchPrices();
-
   }, [selectedPair]);
 
   const formatPrice = (price) => {
-
-    if(selectedPair==="btcEth"){
+    if (selectedPair === 'btcEth') {
       const decimals = 18;
       const formattedPrice = ethers.utils.formatUnits(price, decimals);
       return formattedPrice;
-    }
-    else{
+    } else {
       const decimals = 8;
       const formattedPrice = ethers.utils.formatUnits(price, decimals);
       return formattedPrice;
     }
   };
 
-    return (
-        <Paper className={classes.box}>
-          <Typography variant="h4" gutterBottom>
-            Live Conversion Prices
-          </Typography>
-          <div className={classes.radioButtons}>
-            <RadioGroup row value={selectedPair} onChange={(e) => setSelectedPair(e.target.value)}>
-              <FormControlLabel value="btcUsd" control={<Radio />} label="BTC/USD" />
-              <FormControlLabel value="ethUsd" control={<Radio />} label="ETH/USD" />
-              <FormControlLabel value="linkUsd" control={<Radio />} label="LINK/USD" />
-              <FormControlLabel value="btcEth" control={<Radio />} label="BTC/ETH" />
-            </RadioGroup>
-          </div>
-          <div className={classes.result}>
-            {loading ? (
-                <CircularProgress />
-            ) : prices[selectedPair] !== null ? (
-                <>
-                  <Typography className={classes.label1}>{selectedPair.toUpperCase()} Conversion:</Typography>
-                  <Typography>
-                    1 {selectedPair.toUpperCase().slice(0, -3)} ={' '}
-                    {formatPrice(prices[selectedPair])}{' '}
-                    {selectedPair.toUpperCase().substring(selectedPair.length - 3)}
-                  </Typography>
-                </>
-            ) : (
-                <Typography>No data available.</Typography>
-            )}
-          </div>
-        </Paper>
-    );
-
-}
+  return (
+      <div className={classes.container}>
+        <Card className={classes.card}>
+          <CardHeader title="Live Conversion Prices" style={{ backgroundColor: "lightblue" }} />
+          <CardContent>
+            <div className={classes.radioButtons}>
+              <RadioGroup column value={selectedPair} onChange={(e) => setSelectedPair(e.target.value)}>
+                <FormControlLabel value="btcUsd" control={<Radio />} label="BTC/USD" />
+                <FormControlLabel value="ethUsd" control={<Radio />} label="ETH/USD" />
+                <FormControlLabel value="linkUsd" control={<Radio />} label="LINK/USD" />
+                <FormControlLabel value="btcEth" control={<Radio />} label="BTC/ETH" />
+              </RadioGroup>
+            </div>
+            <div className={classes.result}>
+              {loading ? (
+                  <CircularProgress />
+              ) : prices[selectedPair] !== null ? (
+                  <>
+                    <Typography className={classes.label1}>{selectedPair.toUpperCase()} Conversion:</Typography>
+                    <Typography>
+                      1 {selectedPair.toUpperCase().slice(0, -3)} ={' '}
+                      {formatPrice(prices[selectedPair])}{' '}
+                      {selectedPair.toUpperCase().substring(selectedPair.length - 3)}
+                    </Typography>
+                  </>
+              ) : (
+                  <Typography>No data available.</Typography>
+              )}
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+  );
+};
 
 export default App;
